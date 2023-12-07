66470d2fccdcf546bee144b2a75d5a5650137b11
Tsurkan <mikh.tsurkan@gmail.com>
2023-12-07 18:43:48 +0300

    fix-1

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "catalog"
 
 urlpatterns = [
-    path('main', views.main, name='main'),
+    path('', views.main, name='main'),
     path('contact_info', views.contact_info, name='contact_info'),
     path('contact_form', views.contact_form, name='contact_form')
 ]
