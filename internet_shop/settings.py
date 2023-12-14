5b148674d49341103b8f93f6081dd51e64b85815
Tsurkan <mikh.tsurkan@gmail.com>
2023-12-14 23:57:24 +0300

    init db

--- internet_shop/settings.py
+++ internet_shop/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'internet_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS': {
+            'service': 'internet_shop_service',
+            'passfile': '.pgpass'
+        }
     }
 }
 
