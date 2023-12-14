e1b4ccfd323db99c31a6f735525b3a1a135259e1
Tsurkan <mikh.tsurkan@gmail.com>
2023-12-15 00:13:47 +0300

    started models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
